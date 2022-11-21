59c0ab53608a698cbdc9dbafe25c6f626647d626
yhosutun2491 <yhosutun2491@gmail.com>
2022-11-21 20:17:10 +0800

    install react-router-dom and set 4 main pages routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+import { HomePage,TodoPage, LoginPage, SignUpPage } from './pages'; //引入4個主頁面
+import {BrowserRouter,Routes,Route} from 'react-router-dom' // react路由模組
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<LoginPage/>}/>
+        <Route path="signup" element={<SignUpPage/>}/>
+        <Route path="todo" element={<TodoPage/>}/>
+        <Route path="*" element={<HomePage/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
