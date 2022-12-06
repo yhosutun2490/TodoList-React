d2738f4642100a7edef8db8864f0c0c3c6f48f72
yhosutun2491 <yhosutun2491@gmail.com>
2022-12-06 17:33:32 +0800

    move deploy.yml to workflow file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import './App.scss'
-import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages' // 引入4個主頁面
-import { BrowserRouter, Routes, Route } from 'react-router-dom' // react路由模組
-import { AuthProvider} from './contexts/AuthContext'
+import './App.scss';
+import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages'; // 引入4個主頁面
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // react路由模組
+import { AuthProvider } from './contexts/AuthContext';
+// 新增 basename
+const basename = process.env.PUBLIC_URL;
 
-function App () {
+function App() {
   return (
-    <div className='app'>
-      <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path='login' element={<LoginPage />} />
-          <Route path='signup' element={<SignUpPage />} />
-          <Route path='todo' element={<TodoPage />} />
-          <Route path='*' element={<HomePage />} />
-        </Routes>
-      </AuthProvider>
+    <div className="app">
+      <BrowserRouter basename={basename}>
+        <AuthProvider>
+          <Routes>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            <Route path="todo" element={<TodoPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
